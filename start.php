76f0397c41a0f73fba08794f697334f94d72730f
Enea Dolcini <eneadolcini@gmail.com>
2018-01-29 20:43:53 +0100

    Aggiunta la funzione update
    Da ora con php start.php update è possibile aggiornare la base automaticamente!

--- start.php
+++ start.php
@@ -1,16 +1,33 @@
 <?php
 
+error_reporting(E_ERROR);
+
 if(isset($argv[1]) and $argv[1] == 'background') {
   shell_exec('screen -d -m php start.php');
   echo PHP_EOL . 'getUpdatesBot avviato in background.' . PHP_EOL;
   exit;
 }
 
-error_reporting(E_ERROR);
+if(isset($argv[1]) and $argv[1] == 'update') {
+  echo PHP_EOL . 'Starting updating getUpdatesBot...' . PHP_EOL;
+  try {
+    file_put_contents('_functions.php', curlRequest('GET', 'https://raw.githubusercontent.com/Neneone/getUpdatesBot/master/_functions.php'));
+    file_put_contents('_variables.php', curlRequest('GET', 'https://raw.githubusercontent.com/Neneone/getUpdatesBot/master/_variables.php'));
+    file_put_contents('LICENSE', curlRequest('GET', 'https://raw.githubusercontent.com/Neneone/getUpdatesBot/master/LICENSE'));
+    file_put_contents('README.md', curlRequest('GET', 'https://raw.githubusercontent.com/Neneone/getUpdatesBot/master/README.md'));
+    file_put_contents('start.php', curlRequest('GET', 'https://raw.githubusercontent.com/Neneone/getUpdatesBot/master/start.php'));
+  } catch (\Exception | \Error $e) {
+    echo PHP_EOL . 'Error while trying to update getUpdatesBot: ' . $e->getMessage() . ' on line ' . $e->getLine() . PHP_EOL;
+    file_put_contents('error.log', 'Error while trying to update getUpdatesBot: ' . $e->getMessage() . ' on line ' . $e->getLine());
+    exit;
+  }
+  echo PHP_EOL . 'Bot updated!' . PHP_EOL;
+  exit;
+}
 
 echo PHP_EOL . 'getUpdatesBot is starting...' . PHP_EOL;
 
-$Token      = '12345678:abcdefghij'; #Insert here your API key
+$Token      = '523209487:AAEtYvbeFiGJ2g-2I76c0c1y6EC5NiKW_YQ'; #Insert here your API key
 $API  = 'https://api.telegram.org/bot' . $Token . '/';
 
 if(file_exists('_commands.php') and file_exists('_functions.php')) {
